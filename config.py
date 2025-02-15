dc49d5bceaa5bd783c5e905146c37e553e923d05
movie607999 <telegrambot6079@gmail.com>
2025-02-15 22:54:08 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = "AAHN7bOTRnjStdedu1wtce-7qHbXC4AqBFo"  # তোমার টেলিগ্রাম বটের টোকেন
+RAPIDAPI_KEY = "ca6dbf9407msh61bd2e5c7e991dap1de329jsn47ad675ee76d"  # তোমার RapidAPI IMDb API-এর Key
